57123df98670a9a1dcb334392cfd547ecd5afa7c
laveena95 <laveenalavee256@gmail.com>
2020-05-25 11:52:04 +0530

    first commit

--- resources/views/coordinator/studentRequest.blade.php
+++ resources/views/coordinator/studentRequest.blade.php
@@ -15,7 +15,7 @@
 
 @section('content')
   <section class="wrapper">
-    <h3 style="margin-top:-20px;"><i class="fa fa-users"></i> <b>Student Requests</b> <i class="fa fa-angle-right"></i> <i class="fa fa-bell"><span class="badge bg-warning">{{ $cv->count() }}</span></i> @yield ('title')</h3>
+    <h3 style="margin-top:;"><i class="fa fa-users"></i> <b>Student Requests</b> <i class="fa fa-angle-right"></i> <i class="fa fa-bell"><span class="badge bg-warning">{{ $cv->count() }}</span></i> @yield ('title')</h3>
           <div class="row mb">
             @if($message = Session::get('success'))
               div class="alert alert-success">
